e417e297bfc5647142585ca4fa6665fa52cbea2a
ledroid <leandro.hdsl@gmail.com>
2016-12-23 17:06:55 -0200

    Atualizado receber curso criado e shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { CursosService } from './cursos/cursos.service';
-
-import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
+import { AppComponent }   from './app.component';
+import { CursosModule }   from './cursos/cursos.module';
+import { CursosService }  from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { ReceberCursoCriadoComponent } from './receber-curso-criado/receber-curso-criado.component';
+import { LogService } from './shared/log.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [CursosService],
+  providers: [LogService],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -6,7 +6,8 @@ import { CursosService } from './cursos.service';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
+  providers: [CursosService]
 })
 export class CursosComponent implements OnInit {
 
@@ -20,6 +21,12 @@ export class CursosComponent implements OnInit {
 
   ngOnInit() {
   	this.cursos = this.cursosService.getCursos();
+
+    CursosService.criouNovoCurso.subscribe(
+    //syntaxe do encascrypt
+    curso => this.cursos.push(curso)
+    );
+
   }
 
 }
